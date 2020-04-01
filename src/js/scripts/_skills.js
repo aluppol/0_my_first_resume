f10517113d2890baaa3c3e6133cc18b433216563
Albert <Albert.Y.Luppol@gmail.com>
2020-04-01 16:36:13 +0300

    Box Jump Done 7h 30m

--- src/js/scripts/_skills.js
+++ src/js/scripts/_skills.js
@@ -1,6 +1,6 @@
 class Animations{
 
-    animate({timing, draw, duration}) {
+    _animate({timing, draw, duration}, event) {
 
         let start = performance.now();
       
@@ -16,6 +16,8 @@ class Animations{
       
           if (timeFraction < 1) {
             requestAnimationFrame(animate);
+          } else if(event){
+            document.dispatchEvent(event);
           }
       
         });
@@ -23,12 +25,11 @@ class Animations{
 }
 
 
-class Box {
+class Box{
     constructor(box){
 
-        class Door extends Animations{
+        class Door{
             constructor(door, left = true){
-                super();
                 this.left = left;
                 this.dom = door;
                 this.transform = {
@@ -63,12 +64,14 @@ class Box {
         this.dom = box;
         this.opened = false;
         this.transform = {
-            rotateX : -20,
+            rotateX : 0,    //-20
             rotateY : -80,
             rotateZ :  0,
             scaleY :  1,
+            scaleX :  1,
+            scaleZ :  1,
             translateX :  0,
-            translateY :  0,
+            translateY :  window.innerHeight * 0.4,
             translateZ :  0,
         };
         this.leftDoor = new Door(this.dom.querySelector('.box__side--top-left'));
@@ -77,11 +80,8 @@ class Box {
         this.rebuildDom();
     }
 
-    _constructTransform(){
-        return `rotateX(${this.transform.rotateX}deg) rotateY(${this.transform.rotateY}deg) rotateZ(${this.transform.rotateZ}deg) scaleY(${this.transform.scaleY}) translateX(${this.transform.translateX}px) translateY(${this.transform.translateY}px) translateZ(${this.transform.translateZ}px)`;
-    }
-
     rebuildDom(){
+
         this.dom.style.transform = `${this._constructTransform()}`;
     }
 
@@ -94,126 +94,264 @@ class Box {
         this.leftDoor.close();
         this.rightDoor.close();
     }
+
+    jump(){
+        
+        let speed = 20;
+
+        this.dom.addEventListener('preparedToJump', function (e){
+            // console.log('prepared to jump!');
+
+            accelerate.apply(this);
+
+        }.bind(this), {once: true});
+
+        this.dom.addEventListener('accelerated', function (e){
+            // console.log('accelerated!');
+            flyUp.apply(this);
+            this.dom.style.transformOrigin = "center center -50px";
+
+        }.bind(this), {once: true});
+
+        this.dom.addEventListener('flyedUp', function (e){
+            // console.log('flyedUp!');
+
+            this.dom.style.transformOrigin = "";
+            fallDown.apply(this);
+
+        }.bind(this), {once: true});
+
+        this.dom.addEventListener('falledDown', function (e){
+            // console.log('falledDown!');
+
+            grounding.apply(this);
+
+        }.bind(this), {once: true});
+
+        this.dom.addEventListener('grounded', function (e){
+            // console.log('grounded!');
+
+            groundSwing.apply(this);
+
+        }.bind(this), {once: true});
+
+        this.dom.addEventListener('groundSwinged', function (e){
+            // console.log('All Done!');
+
+        }.bind(this), {once: true});
+
+        
+        prepareToJump.apply(this);
+
+
+        // functions
+
+
+        function groundSwing(){
+
+
+            let timing  = function (timeFraction){
+
+                    let x = 1.5;
+
+                    timeFraction = 1 - timeFraction;
+                    //linear
+                    return Math.pow(2, 10 * (timeFraction - 1)) * Math.cos(20 * Math.PI * x / 3 * timeFraction);
+                },
+
+                draw = (progress)=>{this._deformate(1 - progress);};
+
+
+            this._animate({
+                timing: timing,
+                draw,
+                duration: 100 * speed,
+            }, new Event('groundSwinged'));
+        }
+
+
+        function grounding(){
+
+            
+            let timing  = function (timeFraction){
+
+                return 1-(--timeFraction)*timeFraction*timeFraction*timeFraction;
+            },
+
+            draw = (progress)=>{this._deformate(1 - progress);};
+
+
+            this._animate({
+                timing: timing,
+                draw,
+                duration: 10 * speed,
+            }, new Event('grounded'));
+        }
+
+
+
+        function fallDown(){
+
+            let timing  = function (timeFraction){
+
+                    return timeFraction*timeFraction*timeFraction;
+                },
+
+                draw = (progress)=>{this._translateY(1 - progress);};
+
+
+            this._animate({
+                timing: timing,
+                draw,
+                duration: 50 * speed,
+            }, new Event('falledDown'));
+        }
+
+
+        function flyUp(){
+
+            let timing  = function (timeFraction){
+
+                    return (--timeFraction)*timeFraction*timeFraction+1;
+                },
+
+                draw = (progress)=>{
+                    this._translateY(progress);
+                };
+
+
+            this._animate({
+                timing: timing,
+                draw,
+                duration: 70 * speed,
+            }, new Event('flyedUp'));
+        }
+
+
+        function accelerate(){
+
+            let max = -10;
+
+            let timing  = function (timeFraction){
+
+                    let x = 1.5;
+
+                    timeFraction = 1 - timeFraction;
+                    //linear
+                    return Math.pow(2, 10 * (timeFraction - 1)) * Math.cos(20 * Math.PI * x / 3 * timeFraction);
+                },
+
+                draw = (progress)=>{
+                    this._deformate(1 - progress);
+                   
+                    if(1 - progress <= max || 1 - progress >= 1){
+                        this.dom.dispatchEvent(new Event('accelerated'));
+                    } else{
+                        max = 1 - progress;
+                    }
+                };
+
+
+            this._animate({
+                timing: timing,
+                draw,
+                duration: 100 * speed,
+            });
+        }
+
+
+        function prepareToJump(){
+
+            let timing  = function (timeFraction){
+
+                    return timeFraction*(2-timeFraction);
+                },
+
+                draw = (progress)=>{this._deformate(1 - progress);};
+
+
+            this._animate({
+                timing: timing,
+                draw,
+                duration: 50 * speed,
+            }, new Event('preparedToJump'));
+        }
+    }
+
+    _constructTransform(){
+        return `translateX(${this.transform.translateX}px) translateY(${this.transform.translateY}px) translateZ(${this.transform.translateZ}px) rotateX(${this.transform.rotateX}deg) rotateY(${this.transform.rotateY}deg) rotateZ(${this.transform.rotateZ}deg) scaleY(${this.transform.scaleY}) scaleX(${this.transform.scaleX}) scaleZ(${this.transform.scaleZ})`;
+    }
+
+    _deformate(sc){ //sc (scale) 1 - scale = 1; 0 - min deformation; 2 - max deformation
+
+        let minDef  = 0.6;
+
+            sc = minDef + (1 - minDef) * sc
+
+        let scaleNew = {
+            y: sc,
+            x: 1 + (1 - sc)**2 * (sc > 1 ? -1 : 1),
+            z: 1 + (1 - sc)**2 * (sc > 1 ? -1 : 1),
+        }
+
+        this.transform.scaleX = scaleNew.x;
+        this.transform.scaleY = scaleNew.y;
+        this.transform.scaleZ = scaleNew.z;
+
+        this.rebuildDom();
+    }
+
+    _translateY(y){ // 0 - 1 amount
+
+        let maxY = window.innerHeight * 0.2,
+            minY = -window.innerHeight * 0.4;
+
+        y = Math.abs(maxY - minY) * y + minY;
+
+        this.transform.translateY = -y;
+
+        this.rebuildDom();
+    }
+
+    _animate({timing, draw, duration}, event) {
+
+        let start = performance.now();
+      
+        requestAnimationFrame(function animate(time) {
+          // timeFraction goes from 0 to 1
+          let timeFraction = (time - start) / duration;
+          if (timeFraction > 1) timeFraction = 1;
+      
+          // calculate the current animation state
+          let progress = timing(timeFraction);
+      
+          draw(progress); // draw it
+      
+          if (timeFraction < 1) {
+            requestAnimationFrame(animate.bind(this));
+          } else if(event){
+            this.dom.dispatchEvent(event);
+          }
+      
+        }.bind(this));
+    }
 }
 
 let box = new Box(document.getElementById('skillsBox'));
 
+// setTimeout(()=>{box.open()}, 2000);
 
-setTimeout(()=>{box.open()}, 2000);
+// setTimeout(()=>{box.close()}, 4000);
 
-setTimeout(()=>{box.close()}, 4000);
+box.jump();
 
-tempWrapFn(); // ----------------------temp--------------------------
-
-let setFE, setJS, setOther, setPM, supersetFE, supersetJS, supersetOther, supersetPM, hover;
-
-// box.rebuildDom();
-
-// setUpAnimationElements();
+// box._deformate(1.2);
 
 
-// functions
+let interval = setInterval(()=>{
 
+    box.jump();
 
-function closingBoxAnimation(){
-    alert('Closed!');
-}
+}, 7000);
 
-
-function openBoxAnimation(){
-    alert('Open!');
-}
-
-
-function setBoxTransformation(){
-
-}
-
-
-function setUpAnimationElements(){
-
-    setBoxTransformation();
-
-    box.dom.addEventListener('click', ()=>{
-
-        box.opened = !box.opened;
-        if(box.opened) {
-            openBoxAnimation();
-            return;
-        }
-
-        closingBoxAnimation();
-
-    });
-
-    setUpBoxWaitingAnimations();
-}
-
-
-function animateInfinite({timing, draw, duration}) {
-
-    let start = performance.now();
-  
-    requestAnimationFrame(function animate(time) {
-      // timeFraction goes from 0 to 1
-        let timeFraction = (time - start) / duration;
-
-        if (timeFraction >= 1) {
-            start = time;
-            timeFraction = 0;
-        }
-  
-        // calculate the current animation state
-        let progress = timing(timeFraction);
-  
-        draw(progress); // draw it
-  
-        if (timeFraction < 1) {
-            requestAnimationFrame(animate);
-        }
-  
-    });
-}
-
-
-function animate({timing, draw, duration}) {
-
-    let start = performance.now();
-  
-    requestAnimationFrame(function animate(time) {
-      // timeFraction goes from 0 to 1
-      let timeFraction = (time - start) / duration;
-      if (timeFraction > 1) timeFraction = 1;
-  
-      // calculate the current animation state
-      let progress = timing(timeFraction);
-  
-      draw(progress); // draw it
-  
-      if (timeFraction < 1) {
-        requestAnimationFrame(animate);
-      }
-  
-    });
-}
-
-
-function tempWrapFn(){
-
-    box.dom.hidden = true;
-
-    let skills = document.querySelectorAll('.superset__item'),
-        stack = document.querySelector('.stack');
-
-    skills.forEach(skill=>{
-        skill.classList.add('temp-skill-item-styles');
-        stack.append(skill);
-    });
-
-    let temp = document.createElement('span');
-
-    temp.innerHTML = "Sorry! This section is in production!"
-    temp.classList.add('temp');
-
-    stack.append(temp);
-}
+// tempWrapFn(); // ----------------------temp--------------------------
